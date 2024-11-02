52983a3079c2e72e77b29993e37f55535530fd74
David Rodenas <david.rodenas+gt@gmail.com>
2024-11-02 13:27:25 +0100

    fix watch

--- watch.sh
+++ watch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 jekyll serve --watch &
 sleep 3
-(cd _site; npx lite-server .)
+(cd _site; npx http-server .)
 # (cd _site; npx browser-sync start --server --files "./**/*.*" --reload-delay 1000)
 # npx browser-sync start --proxy "localhost:4000" --files "_site/**/*.*" --reload-delay 1000
